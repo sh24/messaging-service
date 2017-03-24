b525d6f93a5058d101ad13a6edfd055f4b475558
Heidar Bernhardsson <heidarbn@gmail.com>
2017-03-24 17:07:16 +0000

    support multiple clients

--- lib/messaging_service/voodoo_service.rb
+++ lib/messaging_service/voodoo_service.rb
@@ -1,8 +1,17 @@
 module VoodooService
   def self.client
-    VoodooSMS.new(
-      Settings.voodoo_sms.username,
-      Settings.voodoo_sms.password
-    )
+    VoodooSMS.new username: Settings.voodoo_sms.username, password: Settings.voodoo_sms.password
+  end
+
+  def self.clients
+    Settings.sms_providers.voodoo.map do |credentials|
+      voodoo_sms_client username: username, password: password
+    end
+  end
+
+  private
+
+  def voodoo_sms_client(username:, password:)
+    VoodooSMS.new username, password
   end
 end
