4b5b188ec5be582698d33c12e17f4d165ce19baa
Heidar Bernhardsson <heidarbn@gmail.com>
2017-03-27 13:16:27 +0100

    revert previous changes
    But allow username/password to be passed in.

--- lib/messaging_service/voodoo_service.rb
+++ lib/messaging_service/voodoo_service.rb
@@ -1,15 +1,5 @@
 module VoodooService
-  def self.client
-    self.voodoo_client username: Settings.voodoo_sms.username, password: Settings.voodoo_sms.password
-  end
-
-  def self.clients
-    Settings.sms_providers.voodoo.map do |credentials|
-      self.voodoo_client username: credentials.username, password: credentials.password
-    end
-  end
-
-  def self.voodoo_client(username:, password:)
+  def self.client(username: Settings.voodoo_sms.username, password: Settings.voodoo_sms.password)
     VoodooSMS.new username, password
   end
 end
